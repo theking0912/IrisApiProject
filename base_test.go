9ea7db9ce1800cc8fd9cc003d2e1edda7f430a47
theking0912 <376462759@qq.com>
2019-11-07 00:35:19 +0800

    init

---
+++ base_test.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/theking0912/IrisApiProject/database"
+	"github.com/theking0912/IrisApiProject/models"
+
+	"github.com/gavv/httpexpect"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/httptest"
+	"github.com/theking0912/IrisApiProject/config"
+)
+
+var (
+	app         *iris.Application // iris.Applications
+	testRole    *models.Role
+	testPerm    *models.Permission
+	testPermIds []uint
+	testUser    *models.User
+)
+
+//单元测试基境
+func TestMain(m *testing.M) {
+
+	// 初始化app
+	app = newApp()
+
+	baseCase()
+
+	flag.Parse()
+	exitCode := m.Run()
+
+	// 删除测试数据表，保持测试环境
+	database.DB.DropTable("users", "roles", "permissions", &models.OauthToken{})
+
+	os.Exit(exitCode)
+
+}
+
+// 单元测试 login 方法
+func login(t *testing.T, url string, Object interface{}, StatusCode int, Status bool, Msg string, Data map[string]interface{}) (e *httpexpect.Expect) {
+	e = httptest.New(t, app, httptest.Configuration{Debug: config.Conf.Get("app.debug").(bool)})
+	if Data != nil {
+		e.POST(url).WithJSON(Object).
+			Expect().Status(StatusCode).
+			JSON().Object().Values().Contains(Status, Msg, Data)
+	} else {
+		e.POST(url).WithJSON(Object).
+			Expect().Status(StatusCode).
+			JSON().Object().Values().Contains(Status, Msg)
+	}
+
+	return
+}
+
+// 单元测试 create 方法
+func create(t *testing.T, url string, Object interface{}, StatusCode int, Status bool, Msg string, Data map[string]interface{}) (e *httpexpect.Expect) {
+	e = httptest.New(t, app, httptest.Configuration{Debug: config.Conf.Get("app.debug").(bool)})
+	at := GetLoginToken()
+
+	ob := e.POST(url).WithHeader("Authorization", "Bearer "+at.Token).WithJSON(Object).
+		Expect().Status(StatusCode).JSON().Object()
+
+	ob.Value("status").Equal(Status)
+	ob.Value("msg").Equal(Msg)
+
+	for k, v := range Data {
+		ob.Value("data").Object().Value(k).Equal(v)
+	}
+
+	return
+}
+
+// 单元测试 update 方法
+func update(t *testing.T, url string, Object interface{}, StatusCode int, Status bool, Msg string, Data map[string]interface{}) (e *httpexpect.Expect) {
+	e = httptest.New(t, app, httptest.Configuration{Debug: config.Conf.Get("app.debug").(bool)})
+	at := GetLoginToken()
+
+	ob := e.PUT(url).WithHeader("Authorization", "Bearer "+at.Token).WithJSON(Object).
+		Expect().Status(StatusCode).JSON().Object()
+
+	ob.Value("status").Equal(Status)
+	ob.Value("msg").Equal(Msg)
+
+	for k, v := range Data {
+		ob.Value("data").Object().Value(k).Equal(v)
+	}
+
+	return
+}
+
+// 单元测试 getOne 方法
+//func getOne(t *testing.T, url string, StatusCode int, Status bool, Msg string, Data map[string]interface{}) (e *httpexpect.Expect) {
+//	e = httptest.New(t, app, httptest.Configuration{Debug: config.Conf.Get("app.debug").(bool)})
+//	at := GetLoginToken()
+//	if Data != nil {
+//		e.GET(url).WithHeader("Authorization", "Bearer "+at.Token).
+//			Expect().Status(StatusCode).
+//			JSON().Object().Values().Contains(Status, Msg, Data)
+//	} else {
+//		e.GET(url).WithHeader("Authorization", "Bearer "+at.Token).
+//			Expect().Status(StatusCode).
+//			JSON().Object().Values().Contains(Status, Msg)
+//	}
+//
+//	return
+//}
+
+// 单元测试 getMore 方法
+func getMore(t *testing.T, url string, StatusCode int, Status bool, Msg string, Data map[string]interface{}) (e *httpexpect.Expect) {
+	e = httptest.New(t, app, httptest.Configuration{Debug: config.Conf.Get("app.debug").(bool)})
+	at := GetLoginToken()
+	if Data != nil {
+		e.GET(url).WithHeader("Authorization", "Bearer "+at.Token).
+			Expect().Status(StatusCode).
+			JSON().Object().Values().Contains(Status, Msg, Data)
+	} else {
+		e.GET(url).WithHeader("Authorization", "Bearer "+at.Token).
+			Expect().Status(StatusCode).
+			JSON().Object().Values().Contains(Status, Msg)
+	}
+
+	return
+}
+
+// 单元测试 delete 方法
+func delete(t *testing.T, url string, StatusCode int, Status bool, Msg string, Data map[string]interface{}) (e *httpexpect.Expect) {
+	e = httptest.New(t, app, httptest.Configuration{Debug: config.Conf.Get("app.debug").(bool)})
+	at := GetLoginToken()
+
+	e.DELETE(url).WithHeader("Authorization", "Bearer "+at.Token).
+		Expect().Status(StatusCode).
+		JSON().Object().Values().Contains(Status, Msg)
+	fmt.Println(Data)
+	return
+}
+
+/**
+*登陆用户
+*@return   Token 返回登陆后的token
+ */
+func GetLoginToken() models.Token {
+	response, status, msg := models.CheckLogin(
+		config.Conf.Get("test.LoginUserName").(string),
+		config.Conf.Get("test.LoginPwd").(string),
+	)
+
+	// 打印错误信息
+	if !status {
+		fmt.Println(msg)
+	}
+
+	return response
+}
+
+func baseCase() {
+	perm_json := &models.PermissionJson{
+		Name:        "test_update_user",
+		Description: "访客",
+		DisplayName: "访客",
+	}
+
+	testPerm = models.CreatePermission(perm_json)
+	testPermIds = []uint{testPerm.ID}
+
+	role_json := &models.RoleJson{
+		Name:        "test_update_user",
+		Description: "访客",
+		DisplayName: "访客",
+	}
+
+	testRole = models.CreateRole(role_json, testPermIds)
+
+	aul := &models.UserJson{
+		Username: "guest",
+		Name:     "访客",
+		Password: "guest111",
+		RoleID:   testRole.ID,
+	}
+
+	testUser = models.CreateUser(aul)
+}
